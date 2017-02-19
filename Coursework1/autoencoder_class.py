7afebef811a67ccc41abcf3c13b3d7e473fefa17
JulienSiems <juliensiems@gmail.com>
2017-02-19 12:28:43 +0000

    Variational Autoencoder added

--- Coursework1/autoencoder_class.py
+++ Coursework1/autoencoder_class.py
@@ -4,7 +4,7 @@ import tensorflow.examples.tutorials.mnist.input_data as input_data
 import matplotlib.pyplot as plt
 import functools
 
-
+# TO DO: ADD THE REFERENCES
 def xavier_init(fan_in, fan_out, constant = 1):
     with tf.name_scope('xavier'):
         low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
@@ -51,7 +51,7 @@ def define_scope(function, scope=None, *args, **kwargs):
 
 
 
-class Model:
+class Autoencoder:
 
     def __init__(self, image, enc_dimensions = [784, 500, 200, 64], dec_dimensions = [64, 200, 500, 784]):
         self.image = image
@@ -69,10 +69,10 @@ class Model:
         encoder = []
         with tf.name_scope('Encoder'):
             for layer_i, n_output in enumerate(self.enc_dimensions[1:]):
-                with tf.name_scope('enc_layer'+str(layer_i)):
+                with tf.name_scope('enc_layer' + str(layer_i)):
                     n_input = int(current_input.get_shape()[1])
                     W = tf.Variable(xavier_init(n_input, n_output), name = 'weight'+str(layer_i))
-                    b = tf.Variable(xavier_init(1, n_output), name = 'bias'+str(layer_i))
+                    b = tf.Variable(tf.zeros(shape=(1, n_output)), name = 'bias'+str(layer_i))
                     encoder.append(W)
                     current_input = tf.nn.elu(tf.add(tf.matmul(current_input, W), b),
                                               name='enclayer' + str(layer_i))
@@ -80,10 +80,10 @@ class Model:
         # DECODER
         with tf.name_scope('Decoder'):
             for layer_i, n_output in enumerate(self.dec_dimensions[1:]):
-                with tf.name_scope('dec_layer'+str(layer_i)):
+                with tf.name_scope('dec_layer' + str(layer_i)):
                     n_input = int(current_input.get_shape()[1])
                     W = tf.Variable(xavier_init(n_input, n_output), name = 'weight'+str(layer_i))
-                    b = tf.Variable(xavier_init(1, n_output), name = 'bias'+str(layer_i))
+                    b = tf.Variable(tf.zeros(shape=(1, n_output)), name = 'bias'+str(layer_i))
                     encoder.append(W)
                     current_input = tf.nn.elu(tf.add(tf.matmul(current_input, W), b),
                                               name='decclayer' + str(layer_i))
@@ -105,16 +105,16 @@ def main():
     mnist = input_data.read_data_sets('./mnist/', one_hot=True)
     mean_img = np.mean(mnist.train.images, axis=0)
     image = tf.placeholder(tf.float32, [None, 784])
-    model = Model(image)
+    model = Autoencoder(image)
 
     merged_summary = tf.summary.merge_all()
     sess = tf.Session()
-    logpath = '/tmp/tensorflow_logs/example'
+    logpath = '/tmp/tensorflow_logs/example/1'
     test_writer = tf.summary.FileWriter(logpath, graph=tf.get_default_graph())
     #train_writer = tf.summary.FileWriter('/train')
     sess.run(tf.global_variables_initializer())
 
-    for epoch_i in range(10):
+    for epoch_i in range(1):
         test_images = mnist.test.images
         test = np.array([img - mean_img for img in test_images])
         error, summary = sess.run(fetches=[model.error, merged_summary], feed_dict={image: test})
